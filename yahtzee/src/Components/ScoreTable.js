6b737482c90cd7dd08fbd367191ff53a053e262c
Guro <queengurp@gmail.com>
2023-10-03 04:02:35 +0200

    Table now looks correct. Still missing interraction

--- yahtzee/src/Components/ScoreTable.js
+++ yahtzee/src/Components/ScoreTable.js
@@ -3,26 +3,35 @@ import './ScoreTable.css';
 
 
 export default function ScoreTable({numPlayers}) {
+  
+  const upperSection = ["Ones", "Twos", "Threes", "Fours", "Fives", "Sixes", "Bonus"];
+  const lowerSection = ["Three of a kind", "Four of a kind", "Full house", "Small straight", "Large straight", "Yahtzee", "Chance", "Yahtzee Bonus"];
+  const allSections = ["Score Type", ...upperSection, ...lowerSection];
 
-  var rows = 17;
+  var rows = allSections.length;
   var cols = numPlayers + 1;
 
-  const colsNames = ["", ...Array.from({ length: numPlayers}).map((_,i) => `Player ${i + 1}`)];
-  
   return(
-      <div className="table-content">
-          <table style={{marginLeft: '2vw'}}>
-              <tbody>
-                  {Array.from({length: rows}).map((_, i) => (
-                      <tr key={i}>
-                          {Array.from({ length: cols }).map((_, j) => (
-                              <td key={j}>Row {i + 1} Col {j + 1}</td>
-                          ))}
-                      </tr>
-                  ))}
-              </tbody>
-          </table>
-   
-      </div>
+    <div className="table-content">
+      <table style={{marginLeft: '2vw'}}>
+        <tbody>
+          {Array.from({length: rows}).map((_, i) => (
+            <tr key={i}>
+              {Array.from({ length: cols }).map((_, j) => {
+                if (j === 0) {
+                  return <td key={j}>{allSections[i]}</td>;
+                } else {
+                    if (i === 0) {
+                        return <td key={j}>Player {j}'s Score</td>;
+                      } else {
+                        return <td key={j}></td>; // Empty cell or any placeholder content you want
+                      }
+                }
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
